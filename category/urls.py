33b559f050aa2b12db12876e435837b8448363c9
progspac-vnn <vineshnnadar@gmail.com>
2024-11-21 02:58:28 +0530

    Doc upload section added

--- category/urls.py
+++ category/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'category'
 
 urlpatterns = [
-    path("", views.category_view, name="category")
+    path("", views.category_view, name="category"),
+    path('<int:category_id>/', views.category_detail, name='category-detail'),
+    path("<int:category_id>/upload-documents/", views.upload_documents, name="upload-documents"),
 ]
